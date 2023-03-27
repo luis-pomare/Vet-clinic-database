74af02da02deb53c98489deaa8af2e819e884bbc
Luis Pomare <luispomare92@gmail.com>
2023-03-27 12:55:19 -0500

    Insert first 4 animals to the table

--- data.sql
+++ data.sql
@@ -1,5 +1,7 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO animals(name, date_of_birth, weight_kg, neutered, escape_attempts)
+VALUES ('Agumon', '2020-02-03', 10.23, '1', 0),
+('Gabumon', '2018-11-15', 8, '1', 2),
+('Pikachu', '2021-01-7', 15.04, '0', 1),
+('Devimon', '2017-05-12', 11, '1', 5);
