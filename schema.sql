152e566531ae71dab3197f15907f9722308bf075
Luis Pomare <luispomare92@gmail.com>
2023-03-27 11:28:47 -0500

    Create \'animals\' table

--- schema.sql
+++ schema.sql
@@ -1,3 +1,11 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE DATABASE vet_clinic;
+CREATE TABLE animals(
+    id INT,
+    name VARCHAR,
+    date_of_birth DATE,
+    escape_attempts INT,
+    neutered BOOLEAN(1),
+    weight_kg DECIMAL
+);
