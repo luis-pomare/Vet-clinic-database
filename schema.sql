087e16d4e964e7021f869263a9a1d5b93cd1be68
Luis Pomare <luispomare92@gmail.com>
2023-03-28 11:29:00 -0500

    Add id column as primary key

--- schema.sql
+++ schema.sql
@@ -13,3 +13,7 @@ CREATE TABLE animals(
 -- Fix the ID column to be the identity of the table
 ALTER TABLE animals ALTER COLUMN id SET NOT NULL;
 ALTER TABLE animals ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY;
+
+-- Add id column as primary key
+ALTER TABLE animals
+ADD PRIMARY KEY(id);
