cd2a5b2e65db8dc5fa3add27ae51342b47757680
Luis Pomare <luispomare92@gmail.com>
2023-03-28 23:05:31 -0500

    Execute the second group of queries

--- queries.sql
+++ queries.sql
@@ -8,3 +8,36 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered;
 SELECT * FROM animals WHERE name != 'Gabumon';
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+-- Execute the second group of queries
+BEGIN;
+UPDATE animals
+SET species = 'unspecified';
+ROLLBACK;
+
+BEGIN;
+UPDATE animals
+SET species = 'digimon'
+WHERE name LIKE '%mon%';
+
+UPDATE animals
+SET species = 'pokemon'
+WHERE species IS NULL;
+COMMIT;
+
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
+
+BEGIN;
+DELETE FROM animals
+WHERE date_of_birth > '2022-01-01';
+SAVEPOINT s1;
+UPDATE animals
+SET weight_kg = -1*weight_kg;
+ROLLBACK TO s1;
+UPDATE animals
+SET weight_kg = -1*weight_kg
+WHERE weight_kg < 0;
+COMMIT;
+
