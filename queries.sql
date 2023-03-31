e20ac3a53346bf4887b685bc93e93b7fa54202b1
Luis Pomare <luispomare92@gmail.com>
2023-03-31 08:12:18 -0500

    Fix LIKE queries

--- queries.sql
+++ queries.sql
@@ -1,6 +1,6 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * FROM animals WHERE name LIKE '%mon%';
+SELECT * FROM animals WHERE name LIKE '%mon';
 SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
 SELECT name FROM animals WHERE neutered = 'true' and escape_attempts<3;
 SELECT date_of_birth FROM animals WHERE name = 'Pikachu' OR name = 'Agumon';
@@ -18,7 +18,7 @@ ROLLBACK;
 BEGIN;
 UPDATE animals
 SET species = 'digimon'
-WHERE name LIKE '%mon%';
+WHERE name LIKE '%mon';
 
 UPDATE animals
 SET species = 'pokemon'
