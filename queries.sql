ee57b5d21a304612610dc9f44f51e0d2b7cb51b4
dfsalam <dfsalam@gmail.com>
2023-04-03 14:22:46 -0500

    Use EXPLAIN ANALYZE to check the queries performance.

--- queries.sql
+++ queries.sql
@@ -169,3 +169,5 @@ ORDER BY COUNT(animals.id) DESC LIMIT 1;
 
 -- Queries for performance measurement
 EXPLAIN ANALYZE SELECT COUNT(*) FROM visits where animal_id = 4;
+EXPLAIN ANALYZE SELECT * FROM visits where vet_id = 2;
+EXPLAIN ANALYZE SELECT * FROM owners where email = 'owner_18327@mail.com';
